950daff888711bbac7da20c7f86a751a3dfc4a3f
Karanveer Masoun <masounkaranveer@gmail.com>
2025-03-04 16:26:48 -0500

    bin commit for ICE 06

--- eclipse-workspace/Time2/src/time/Time.java
+++ eclipse-workspace/Time2/src/time/Time.java
@@ -21,7 +21,6 @@ import javax.swing.JOptionPane;
  */
 public class Time
 {
-	
 	public static void main(String[] args) {
 		int totalSeconds = getTotalSeconds("10:10:10");
 		System.out.println("Github Seconds = "+totalSeconds);
