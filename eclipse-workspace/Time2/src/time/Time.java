21a3422862553b403150994875baa1e3d4fa1fcf
Karanveer Masoun <masounkaranveer@gmail.com>
2025-03-04 16:09:40 -0500

    bin

--- eclipse-workspace/Time2/src/time/Time.java
+++ eclipse-workspace/Time2/src/time/Time.java
@@ -21,6 +21,7 @@ import javax.swing.JOptionPane;
  */
 public class Time
 {
+	
 	public static void main(String[] args) {
 		int totalSeconds = getTotalSeconds("10:10:10");
 		System.out.println("Github Seconds = "+totalSeconds);
